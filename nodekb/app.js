991efbf2a16ffe889635256b97acd9664f3b3f14
Unknown <axel.calo@gmail.com>
2017-12-08 00:13:30 -0400

    Testing again

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const  path = require ('path');
+const ejs = require('ejs');
 
 //Init App
 const app = express();
 
 //Load View engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 //Home Route
 app.get('/', function(req, res){
@@ -16,9 +18,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/testing',function(req,res){
-  res.render('testing',{
-    title:'The test'
-  });
+  res.render('testing');
 });
 
 //Start Server
